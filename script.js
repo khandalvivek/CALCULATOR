aee44b2d22ad594f3dd8db016c463284165c9442
VIVEK KHANDAL <123463395+khandalvivek@users.noreply.github.com>
2023-12-23 11:32:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll('button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (event)=>{
+
+        if(event.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(event.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(event.target.innerHTML == 'DE'){
+            string = string.toString().slice(0, -1)
+            document.querySelector('input').value = string;
+        }
+        else{
+        string = string + event.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
